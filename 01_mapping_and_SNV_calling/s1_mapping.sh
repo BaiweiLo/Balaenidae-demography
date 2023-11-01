cef8f0e01e641a23030a7835f02a0b403376fa6a
JohnsonStev <38938938+JohnsonStev@users.noreply.github.com>
2023-11-01 19:47:54 +0100

    Update s1_mapping.sh

--- 01_mapping_and_SNV_calling/s1_mapping.sh
+++ 01_mapping_and_SNV_calling/s1_mapping.sh
@@ -1,11 +1,18 @@
-ref=phyMac1_rm.fa
-sp=BW
+ref=reference.fa
+sp=species
 
-read1=/project/MDL_ChIPseq/Fastq/baiwei/whale/bluewhale/055666_L999_R1_001.fastq.gz
-read2=/project/MDL_ChIPseq/Fastq/baiwei/whale/bluewhale/055666_L999_R2_001.fastq.gz
+read1=R1.fq.gz
+read2=R2.fq.gz
 
+# mapping
 bwa mem -t 30 $ref $read1 $read2 | samtools sort > ${sp}_sorted.bam
 
+# add read groups
 gatk AddOrReplaceReadGroups -I ${sp}_sorted.bam -O ${sp}_withGroup_sorted.bam -LB bar -PL illumina -PU run -SM $sp -SO coordinate
 
 samtools index ${sp}_withGroup_sorted.bam
+
+# downsampling
+read_depth_ratio_to_SRW=number
+
+samtools view -s $read_depth_ratio_to_SRW -b ${sp}_withGroup_sorted.bam > ${sp}_subsampled.bam
