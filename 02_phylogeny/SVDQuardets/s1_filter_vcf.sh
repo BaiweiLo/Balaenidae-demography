db418fd45855c244689d19b4acff8ae55b1c9008
JohnsonStev <38938938+JohnsonStev@users.noreply.github.com>
2023-11-01 20:49:26 +0100

    Update s1_filter_vcf.sh

--- 02_phylogeny/SVDQuardets/s1_filter_vcf.sh
+++ 02_phylogeny/SVDQuardets/s1_filter_vcf.sh
@@ -1,2 +1,2 @@
 # filter vcf so that all remaining SNVs are at least 100bp apart
-vcftools --gzvcf $vcf  --thin 100 --recode --out $vcf-for_SVDQuardets
+vcftools --gzvcf $vcf  --thin 100 --recode --out $vcf-for_SVDQuartets
