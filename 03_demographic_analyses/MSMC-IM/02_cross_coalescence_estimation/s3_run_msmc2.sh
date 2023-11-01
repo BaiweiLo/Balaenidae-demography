9de63eff9cbe158b9aa8bff0eb0271a22887a272
JohnsonStev <38938938+JohnsonStev@users.noreply.github.com>
2023-11-01 20:40:31 +0100

    Update s3_run_msmc2.sh

--- 03_demographic_analyses/MSMC-IM/02_cross_coalescence_estimation/s3_run_msmc2.sh
+++ 03_demographic_analyses/MSMC-IM/02_cross_coalescence_estimation/s3_run_msmc2.sh
@@ -1,1 +1,6 @@
 # like msmc on single species, this time the input are cross species msmc file
+
+msmc2_linux64bit -o ${sp1}_${sp2}_msmc2
+${sp1}_${sp2}_${chr1}.msmc \
+${sp1}_${sp2}_${chr2}.msmc \
+...
