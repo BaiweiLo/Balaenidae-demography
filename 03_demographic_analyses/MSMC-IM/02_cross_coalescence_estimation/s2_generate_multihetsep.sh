274c1b3aaeba46bc6bae178ad47c8f00552c1860
JohnsonStev <38938938+JohnsonStev@users.noreply.github.com>
2023-11-01 20:37:41 +0100

    Update s2_generate_multihetsep.sh

--- 03_demographic_analyses/MSMC-IM/02_cross_coalescence_estimation/s2_generate_multihetsep.sh
+++ 03_demographic_analyses/MSMC-IM/02_cross_coalescence_estimation/s2_generate_multihetsep.sh
@@ -3,4 +3,4 @@ sp2=species2
 
 # Do this for every chromosome
 # using masked files generates in 01_MSMC
-msmc-tools/generate_multihetsep.py --mask=${sp1}_${chr1}_out_mask.bed.gz --mask=${sp2}_${chr1}_out_mask.bed.gz 
+msmc-tools/generate_multihetsep.py --mask=${sp1}_${chr1}_out_mask.bed.gz --mask=${sp2}_${chr1}_out_mask.bed.gz ${sp1}_${chr1}_phased.vcf.gz ${sp2}_${chr1}_phased.vcf.gz > ${sp1}_${sp2}/${sp1}_${sp2}_${chr1}.msmc
