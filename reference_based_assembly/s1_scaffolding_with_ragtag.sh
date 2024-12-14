0358083ca79301ba737e695c2ad8998a04cd2fdd
Bai-Wei Lo <38938938+BaiweiLo@users.noreply.github.com>
2024-12-14 19:29:16 +0100

    Update s1_scaffolding_with_ragtag.sh

--- reference_based_assembly/s1_scaffolding_with_ragtag.sh
+++ reference_based_assembly/s1_scaffolding_with_ragtag.sh
@@ -1,3 +1,3 @@
-# use chromosome level aseembly from closely related species to do scaffolding
+# use a chromosome level assembly of a closely related species: chromosomes.fa to do scaffolding for a contig/scaffold level assembly: scaffolds.fa
 
-ragtag.py scaffold -C -r --remove-small $chromosomes.fa $scaffolds.fa
+ragtag.py scaffold -C -r --remove-small chromosomes.fa scaffolds.fa
